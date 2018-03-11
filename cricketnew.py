c8dd29216c17186928888bd83216b555341d615d
dumpalasaiavinash <36899352+dumpalasaiavinash@users.noreply.github.com>
2018-03-11 22:06:10 +0530

    cricket

---
+++ cricketnew.py
@@ -0,0 +1,125 @@
+import requests
+import webbrowser
+from gtts import gTTS
+import os
+
+
+link="http://www.cricbuzz.com/"
+new=2
+#webbrowser.open(link,new=new)
+
+f = requests.get(link)
+
+#print(f.text)
+a=f.text
+b=a.find('cb-col cb-col-25 cb-mtch-blk')
+#print(b)
+team=""
+for i in range(40,100):
+    if  f.text[b+i] == '"':
+        break
+    team=team + f.text[b+i]
+
+print(team)
+team1=""
+for i in range(282,800):
+    if  f.text[b+i] == '<':
+        break
+    team1=team1 + f.text[b+i]
+print(team1)
+
+score1=""
+for i in range(357,800):
+    if  f.text[b+i] == '<' or f.text[b+i] == 'O':
+        break
+    score1=score1 + f.text[b+i]
+print(score1)
+
+
+team2=""
+for i in range(460,800):
+    if  f.text[b+i] == '<':
+        break
+    team2=team2 + f.text[b+i]
+print(team2)
+
+score2=""
+for i in range(535,800):
+    if  f.text[b+i] == '<':
+        break
+    score2=score2 + f.text[b+i]
+print(score2)
+if team1=="RSA":
+    team1 = "southafrica"
+if team1=="AUS":
+    team1 = "australia"
+if team1=="BAN":
+    team1 = "bangladesh"
+if team1=="SL":
+    team1 = "srilanka"
+if team1=="NZ":
+    team1 = "new zealand"
+if team1=="ENG":
+    team1 = "england"
+if team1=="ZIM":
+    team1 = "zimbabwe"
+if team1=="IRE":
+    team1 = "ireland"
+if team1=="WI":
+    team1 = "west indies"
+if team1=="AUS":
+    team1 = "australia"
+if team1=="PAK":
+    team1 = "pakistan"
+if team1=="AFG":
+    team1 = "afghanistan"
+if team1=="IND":
+    team1 = "india"
+
+
+
+if team2=="RSA":
+    team2 = "south africa"
+if team2=="AUS":
+    team2 = "australia"
+if team2=="BAN":
+    team2 = "bangladesh"
+if team2=="SL":
+    team2 = "srilanka"
+if team2=="NZ":
+    team2 = "new zealand"
+if team2=="ENG":
+    team2 = "england"
+if team2=="ZIM":
+    team2 = "zimbabwe"
+if team2=="IRE":
+    team2 = "ireland"
+if team2=="WI":
+    team2 = "west indies"
+if team2=="AUS":
+    team2 = "australia"
+if team2=="PAK":
+    team2 = "pakistan"
+if team2=="AFG":
+    team2 = "afghanistan"
+if team2=="IND":
+    team2 = "india"
+
+
+
+team = team.replace(" v "," vs ")
+score1=score1.replace("/"," for ")
+score1=score1.replace("."," point ")
+score1=score1.replace("("," in ")
+
+score2=score2.replace("/"," for ")
+score2=score2.replace("."," point ")
+score2=score2.replace("("," in ")
+team2=team2.replace("RSA","southafrica")
+team2=team2.replace(">","")
+
+output = team + "    "+  team1  + "    "+ "scored" + "    "+ score1  + "overs" + "       "+ team2 + "    " + "scored" + "     "+ score2
+
+tts = gTTS(text=output, lang='en-us')
+tts.save("hel.mp3")
+os.system("hel.mp3")
